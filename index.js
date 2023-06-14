e1298035473dfc02e1701630f30b4fc75235a470
whoisYeshua <alien19987@gmail.com>
2023-06-14 18:13:23 +0300

    new2

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ import { startStandaloneServer } from '@apollo/server/standalone'
 import { addMocksToSchema } from '@graphql-tools/mock'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 
-const currentDirectory = new URL(import.meta.url).pathname.split('/').slice(0, -1).join('/')
-const typeDefsPath = resolve(currentDirectory, '../schema.gql')
+const typeDefsPath = resolve('./schema.gql')
 const typeDefs = readFileSync(typeDefsPath).toString('utf-8')
 
 const resolvers = {
