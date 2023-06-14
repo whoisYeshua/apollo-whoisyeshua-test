dc5ae7f7b0aa76e2906a7643cfabdd43fb9279fd
whoisYeshua <alien19987@gmail.com>
2023-06-14 13:17:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import { readFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+import { addMocksToSchema } from '@graphql-tools/mock'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+
+const typeDefsPath = resolve('./schema.gql')
+const typeDefs = readFileSync(typeDefsPath).toString('utf-8')
+
+const resolvers = {
+  Query: {},
+}
+
+const mocks = {
+  Int: () => 6,
+  Float: () => 22.1,
+  String: () => 'Hello',
+}
+
+const server = new ApolloServer({
+  schema: addMocksToSchema({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    mocks,
+  }),
+})
+
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } })
+
+console.log(`🚀 Server listening at: ${url}`)
